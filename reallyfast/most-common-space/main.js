98b044a7fdd26c55635d69b7dae97551ab8ecfed
ellieeet123 <75391956+ellieeet123@users.noreply.github.com>
2021-12-31 15:14:03 -0600

    idk

--- reallyfast/most-common-space/main.js
+++ reallyfast/most-common-space/main.js
@@ -3,13 +3,6 @@
 
 var players = [0,0,0,0]; // one for each player, the number is 1-100 for the player space, 0 means that player is not yet on the board (has not moved yet)
 var wins = [0,0,0,0];
-var totalroundcount = [];
-var spacecount = {};
-var spacecountlist = [];
-for (var i = 0; i < 101; i++) {
-    spacecount[i] = 0;
-    spacecountlist[i] = 0;
-}
 var specials = {
     1: 38,
     4: 14,
@@ -31,67 +24,42 @@ var specials = {
     95: 75,
     98: 78
 };
-var over100 = 0;
+
 function runRound() {
-    // might be a good idea to remove this loop to increase speed
     for (var i = 0; i < 4; i++) {
         players[i] += (Math.floor(Math.random() * 6) + 1);
         if (specials[players[i]] != undefined) {
             players[i] = specials[players[i]];
         }
-        if (players[i] >= 100) {
+        if (players[i] > 100) {
             players[i] = 100;
-            spacecount[players[i]] += 1;
-            spacecountlist[players[i]] += 1;
             return [true, i];
-        }
-        else {
-            spacecount[players[i]] += 1;
-            spacecountlist[players[i]] += 1;
         }
     }
     return [false];
 }
 
-var roundcount = 0;
 function main(games) { // games is the number of games to play
     for (var j = 0; j < 100; j++) {
         for (var i = 0; i < (games / 100); i++) {
             players = [0,0,0,0];
-            roundcount = 0;
             while (true) {
                 var result = runRound();
-                roundcount++;
                 if (result[0]) {
                     wins[result[1]] += 1;
-                    if (totalroundcount[roundcount] != undefined) {
-                        totalroundcount[roundcount] += 1;
-                    }
-                    else {
-                        totalroundcount[roundcount] = 1;
-                    }
                     break;
                 }
             }
         }
-        console.log('still running... ' + (j + 1).toString() + '%');
+        console.log('still running... ' + j.toString() + '%');
     }
 }
 
-var games = 1000000;
+var games = 10000000;
 main(games);
-console.log('finished simulations, calculating results...');
-console.log('');
-console.log('Total games simulated: ' + games);
 console.log(wins);
 console.log('RED: ' + ((wins[0]/games)*100).toFixed(2) + '%');
 console.log('YELLOW: ' + ((wins[1]/games)*100).toFixed(2) + '%');
 console.log('GREEN: ' + ((wins[2]/games)*100).toFixed(2) + '%');
 console.log('BLUE: ' + ((wins[3]/games)*100).toFixed(2) + '%');
-console.log('');
-console.log('spacecount:');
-console.log(spacecount);
-console.log(Math.max(...spacecountlist));
-console.log('');
-console.log('Rounds:');
-console.log(totalroundcount);
+
